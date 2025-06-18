1e6288da594025fa2e3818dbfdb092069359d09c
xjeyceex <jcmiguel.beltran@gmail.com>
2025-06-18 18:51:22 +0800

    c

--- android/app/src/main/java/com/jcmiguel/pesowise/MainActivity.java
+++ android/app/src/main/java/com/jcmiguel/pesowise/MainActivity.java
@@ -1,5 +1,34 @@
 package com.jcmiguel.pesowise;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Display display = getWindowManager().getDefaultDisplay();
+            Display.Mode[] modes = display.getSupportedModes();
+            Display.Mode bestMode = null;
+            float highestRefreshRate = 0f;
+
+            for (Display.Mode mode : modes) {
+                if (mode.getRefreshRate() > highestRefreshRate) {
+                    highestRefreshRate = mode.getRefreshRate();
+                    bestMode = mode;
+                }
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && bestMode != null) {
+                WindowManager.LayoutParams params = getWindow().getAttributes();
+                params.preferredDisplayModeId = bestMode.getModeId();
+                getWindow().setAttributes(params);
+            }
+        }
+    }
+}
